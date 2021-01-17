44d1b6f9f54be53ac4f6cbc7fb737286c8fe6aea
Piyush <impiyush016@gmail.com>
2021-01-17 10:20:14 +0000

    Sync LeetCode submission - Decode XORed Array (python3)

---
+++ problems/decode_xored_array/solution.py
@@ -0,0 +1,7 @@
+class Solution:
+    def decode(self, encoded: List[int], first: int) -> List[int]:
+        res=[first]*(len(encoded)+1)
+        for i in range(len(encoded)):
+            res[i+1]=encoded[i]^res[i]
+        return res
+        
