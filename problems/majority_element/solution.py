098972f65b2663e5f10aa67f613e0fee0a3fe507
Piyush <impiyush016@gmail.com>
2021-02-17 14:28:53 +0000

    Sync LeetCode submission - Majority Element (python3)

---
+++ problems/majority_element/solution.py
@@ -0,0 +1,31 @@
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        '''from collections import Counter
+        lst=Counter(nums)
+        mj=len(nums)//2
+        for i in lst:
+            if lst[i]>mj:
+                return i'''
+        
+        #Boyer-Moore:
+        def bm():
+            cnt=0
+            for i in nums:
+                if cnt==0:
+                    ans=i
+                cnt+=(1 if i==ans else -1)
+            return ans
+        return bm()
+                
+        def solve(lo,hi):
+            mid=lo+((hi-lo)//2)
+            if lo==hi:
+                return nums[lo]
+            leftmj=solve(lo,mid)
+            rightmj=solve(mid+1,hi)
+            if leftmj==rightmj:
+                return leftmj
+            lftcnt=sum(1 for i in range(lo,hi+1) if nums[i]==leftmj)
+            rtcnt=sum(1 for i in range(lo,hi+1) if nums[i] == rightmj)
+            return leftmj if lftcnt>rtcnt else rightmj
+        return solve(0,len(nums)-1)
