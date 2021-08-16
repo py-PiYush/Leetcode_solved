1d0e8dd600a76b44120444221d785e1f821511cd
Piyush <impiyush016@gmail.com>
2021-08-16 14:56:07 +0000

    Sync LeetCode submission - Max Consecutive Ones (python3)

--- problems/max_consecutive_ones/solution.py
+++ problems/max_consecutive_ones/solution.py
@@ -1,11 +1,11 @@
-from itertools import groupby
 class Solution:
     def findMaxConsecutiveOnes(self, nums: List[int]) -> int:
-        res=[]
-        for i,j in groupby(nums):
-            if int(i)==1:
-                res.append(len(list(j)))
-        if len(res)==0:
-            return 0
-        return max(res)
+        cnt, max_=0,0
+        for i in nums:
+            if i==1:
+                cnt+=1
+                max_=max(max_, cnt)
+            else:
+                cnt=0
+        return max_
         
