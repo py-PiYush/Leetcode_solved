36538c9c97db0ad666f7928dd2fdd4fafb5e90f1
Piyush <impiyush016@gmail.com>
2021-09-28 15:16:01 +0000

    Sync LeetCode submission - Copy List with Random Pointer (python3)

---
+++ problems/copy_list_with_random_pointer/solution.py
@@ -0,0 +1,25 @@
+import collections
+
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, x: int, next: 'Node' = None, random: 'Node' = None):
+        self.val = int(x)
+        self.next = next
+        self.random = random
+"""
+
+class Solution:
+    def copyRandomList(self, head: 'Node') -> 'Node':
+        """dict with old Nodes as keys and new Nodes as values. Doing so allows us to create node's next and random as we iterate through the list from head to tail. Otherwise, we need to go through the list backwards.
+        defaultdict() is an efficient way of handling missing keys """ 
+        map_new = collections.defaultdict(lambda: Node(0, None, None))
+        map_new[None] = None # if a node's next or random is None, their value will be None but not a new Node,                                 doing so removes the if-else check in the while loop
+        
+        nd_old = head
+        while nd_old:
+            map_new[nd_old].val = nd_old.val
+            map_new[nd_old].next = map_new[nd_old.next]
+            map_new[nd_old].random = map_new[nd_old.random]
+            nd_old = nd_old.next
+        return map_new[head]
