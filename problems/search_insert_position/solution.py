92ec417d5aaf63dd5f68faaf3476714b1bb6df9f
Piyush <impiyush016@gmail.com>
2021-10-03 05:02:36 +0000

    Sync LeetCode submission - Search Insert Position (python3)

--- problems/search_insert_position/solution.py
+++ problems/search_insert_position/solution.py
@@ -1,12 +1,23 @@
 class Solution:
     def searchInsert(self, nums: List[int], target: int) -> int:
-        try:
-            return nums.index(target)
-        except:
-            for i in range(len(nums)):
-                if nums[i]>target:
-                    return i
-            return len(nums)
+        # try:
+        #     return nums.index(target)
+        # except:
+        #     for i in range(len(nums)):
+        #         if nums[i]>target:
+        #             return i
+        #     return len(nums)
+        
+        l,r=0,len(nums)-1
+        while l<=r:
+            mid=(l+r)//2
+            if nums[mid]==target:
+                return mid
+            elif nums[mid]<target:
+                l=mid+1
+            else:
+                r=mid-1
+        return l
             
             
             
