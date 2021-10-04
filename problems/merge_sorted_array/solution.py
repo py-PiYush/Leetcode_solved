0f2006a517070fc6bed1073cf5f8caed373f1b2d
Piyush <impiyush016@gmail.com>
2021-10-04 04:36:56 +0000

    Sync LeetCode submission - Merge Sorted Array (python3)

--- problems/merge_sorted_array/solution.py
+++ problems/merge_sorted_array/solution.py
@@ -4,5 +4,21 @@ class Solution:
         Do not return anything, modify nums1 in-place instead.
         
         """
-        nums1[m:] = nums2[:n]
-        nums1.sort()
+        # nums1[m:] = nums2[:n]
+        # nums1.sort()
+        
+        temp=nums1[:m]
+        p1,p2,final=0,0,0
+        while p1<len(temp) and  p2<len(nums2):
+            if temp[p1]<=nums2[p2]:
+                nums1[final]=temp[p1]
+                p1+=1
+                final+=1
+            elif temp[p1]>nums2[p2]:
+                nums1[final]=nums2[p2]
+                p2+=1
+                final+=1
+        if p1<len(temp):
+            nums1[final:]=temp[p1:]
+        elif p2<len(nums2):
+            nums1[final:]=nums2[p2:]
