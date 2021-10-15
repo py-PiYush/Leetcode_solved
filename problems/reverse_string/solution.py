b172b285186ed7517a201776478b75908f073cfc
Piyush <impiyush016@gmail.com>
2021-10-15 11:47:02 +0000

    Sync LeetCode submission - Reverse String (python3)

--- problems/reverse_string/solution.py
+++ problems/reverse_string/solution.py
@@ -7,9 +7,19 @@ class Solution:
         # for i in range(len(s)):
         #     s[i]=new[i]
         
-        start,end=0,len(s)-1
-        while start < end:
-            s[start],s[end]=s[end],s[start]
-            start+=1
-            end-=1
+        '''Iterative'''
+        # start,end=0,len(s)-1
+        # while start < end:
+        #     s[start],s[end]=s[end],s[start]
+        #     start+=1
+        #     end-=1
         
+        '''Recursive'''
+        def recurse(s, start, end):
+            if start>=end:
+                return
+            s[start], s[end]=s[end], s[start]
+            recurse(s, start+1, end-1)
+            
+        start, end=0, len(s)-1
+        recurse(s, start, end)
