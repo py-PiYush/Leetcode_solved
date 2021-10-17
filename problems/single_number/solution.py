77efcff314751860940e4e67e617bd44f9f91fe8
Piyush <impiyush016@gmail.com>
2021-10-17 02:56:59 +0000

    Sync LeetCode submission - Single Number (python3)

--- problems/single_number/solution.py
+++ problems/single_number/solution.py
@@ -1,3 +1,23 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        return 2*sum(set(nums))-sum(nums)
+        
+        '''Usig XOR'''
+        # xor of a number with itself returns 0
+#         res=0
+#         for i in nums:
+#             res^=i
+#         return res
+        
+        return reduce(lambda x,y: x^y, nums)
+        
+        
+        '''Extra Space'''
+        #return 2*sum(set(nums))-sum(nums)
+        
+        #Using dictionary
+        d=dict()
+        for i in nums:
+            d[i]=d.get(i,0)+1
+        for key,value in d.items():
+            if value==1:
+                return key
