7dd249abda757207d3e8075d68fedc6707718a36
Piyush <impiyush016@gmail.com>
2021-12-06 05:37:23 +0000

    Sync LeetCode submission - Maximum Subarray (python3)

--- problems/maximum_subarray/solution.py
+++ problems/maximum_subarray/solution.py
@@ -1,18 +1,14 @@
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
-#         result, curSum = nums[0], 0
+        result, curSum = nums[0], 0
         
-#         for i in range(len(nums)):
-#             if curSum < 0:
-#                 curSum = 0
-#             curSum += nums[i]
-#             result = max(result, curSum)
+        for i in range(len(nums)):
+            if curSum < 0:
+                curSum = 0
+            curSum += nums[i]
+            result = max(result, curSum)
         
-#         return result
-        
-        # for i in range(1,len(nums)):
-        #     nums[i]=max(nums[i], nums[i]+nums[i-1])
-        # return max(nums)
+        return result
         
         
         '''Divide and Conquer (As given in CLRS)'''
