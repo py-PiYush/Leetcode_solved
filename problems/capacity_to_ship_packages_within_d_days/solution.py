05fc93fbe93a8f3a75a8dffc92db24fff3fd8c6d
Piyush <impiyush016@gmail.com>
2021-12-06 11:33:35 +0000

    Sync LeetCode submission - Capacity To Ship Packages Within D Days (python3)

---
+++ problems/capacity_to_ship_packages_within_d_days/solution.py
@@ -0,0 +1,22 @@
+class Solution:
+    def shipWithinDays(self, weights: List[int], days: int) -> int:
+        def feasible(capacity):
+            daysTaken=1
+            total=0
+            for weight in weights:
+                total+=weight
+                if total>capacity:
+                    total=weight
+                    daysTaken+=1
+                    if daysTaken>days:
+                        return False
+            return True
+        
+        left,right=max(weights),sum(weights)
+        while left<right:
+            mid=left+ (right-left)//2
+            if feasible(mid):
+                right=mid
+            else:
+                left=mid+1
+        return left
