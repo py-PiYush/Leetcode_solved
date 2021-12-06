d6413fb3d97cca04dfc391046c766ffdebdf9d72
Piyush <impiyush016@gmail.com>
2021-12-06 14:24:16 +0000

    Sync LeetCode submission - Sort Colors (python3)

--- problems/sort_colors/solution.py
+++ problems/sort_colors/solution.py
@@ -3,14 +3,37 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        def insertion(nums):
-            for i in range(1,len(nums)):
-                key=nums[i]
-                j=i-1
-                while j>=0 and key<nums[j]:
-                    nums[j+1]=nums[j]
-                    j-=1
-                nums[j+1]=key
+        self.dnf(nums)
+        
+    def dnf(self,nums):
+        def swap(nums,i,j):
+            nums[i],nums[j]=nums[j],nums[i]
             
-        insertion(nums)
+        start,white=0,0
+        pivot=1
+        end=len(nums)-1
+        while white<=end:
+            if nums[white]<pivot:
+                swap(nums,start,white)
+                start+=1
+                white+=1
+                
+            elif nums[white]>pivot:
+                swap(nums,white,end)
+                end-=1
+            else:
+                white+=1
+                
+                
+            
+#         def insertion(nums):
+#             for i in range(1,len(nums)):
+#                 key=nums[i]
+#                 j=i-1
+#                 while j>=0 and key<nums[j]:
+#                     nums[j+1]=nums[j]
+#                     j-=1
+#                 nums[j+1]=key
+            
+#         insertion(nums)
                     
