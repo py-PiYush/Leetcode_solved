a302df509ec4f3c30775208ca1f4423d49f4d474
Piyush <impiyush016@gmail.com>
2021-12-07 11:14:11 +0000

    Sync LeetCode submission - Rotate Array (python3)

--- problems/rotate_array/solution.py
+++ problems/rotate_array/solution.py
@@ -3,14 +3,27 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        '''Approach 1'''
+        '''Approach 1--> O(n)'''
 #         k=k%len(nums)
 #         result=nums[len(nums)-k:]+nums[:len(nums)-k]
 #         for i in range(len(nums)):
 #             nums[i]=result[i]
         
-        '''Approach 2'''
-        k=k%len(nums)
-        for _ in range(k):
-            last=nums.pop()
-            nums.insert(0,last)
+        '''Approach 2--> O(n*k)'''
+        # k=k%len(nums)
+        # for _ in range(k):
+        #     last=nums.pop()
+        #     nums.insert(0,last)
+        
+        
+        '''Recursion'''
+        def numReverse(start, end):
+            while start < end:
+                nums[start], nums[end] = nums[end], nums[start]
+                start += 1
+                end -= 1
+        k, n = k % len(nums), len(nums)
+        if k:
+            numReverse(0, n - 1)
+            numReverse(0, k - 1)
+            numReverse(k, n - 1)
