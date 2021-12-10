05641ea082eaf786235939e0c6612ad85743638a
Piyush <impiyush016@gmail.com>
2021-12-10 03:13:44 +0000

    Sync LeetCode submission - Domino and Tromino Tiling (python3)

---
+++ problems/domino_and_tromino_tiling/solution.py
@@ -0,0 +1,14 @@
+class Solution:
+    def numTilings(self, n: int) -> int:
+        MOD = 1_000_000_007
+        if n <= 2:
+            return n
+        fCurrent = 5
+        fPrevious = 2
+        fBeforePrevious = 1
+        for k in range(4, n + 1):
+            tmp = fPrevious 
+            fPrevious = fCurrent
+            fCurrent = (2 * fCurrent + fBeforePrevious) % MOD
+            fBeforePrevious = tmp
+        return fCurrent
