2ffe7ebf412c4ddbf68298ac9c0833efbc298a71
Piyush <impiyush016@gmail.com>
2021-12-13 12:18:35 +0000

    Sync LeetCode submission - The K Weakest Rows in a Matrix (python3)

--- problems/the_k_weakest_rows_in_a_matrix/solution.py
+++ problems/the_k_weakest_rows_in_a_matrix/solution.py
@@ -1,14 +1,39 @@
 class Solution:
     def kWeakestRows(self, mat: List[List[int]], k: int) -> List[int]:
-        '''res=[]
-        for i in range(len(mat)):
-            res.append([i,sum(mat[i])])
-        final= sorted(res,key=lambda x:x[1])
-        return [i[0]for i in final][:k]'''
-        scores = list(map(lambda l: sum(l), mat))
-        scores_enum = list(enumerate(scores))
-        print(scores_enum)
         
-        ordered = sorted(scores_enum, key=lambda el: el[1])
+        '''Binary search + heap'''
+        import heapq
+        def count_ones(arr):
+            lo,hi=0,len(arr)
+            while lo<hi:
+                mid=lo+ (hi-lo >> 1)
+                if arr[mid]==1:
+                    lo=mid+1
+                else:
+                    hi=mid
+            return lo
         
-        return [ordered[i][0] for i in range(k)]
+        oneIdx=[[count_ones(row),i] for i,row in enumerate(mat)]
+        print(heapq.nsmallest(k, oneIdx))
+        return [ res[1] for res in heapq.nsmallest(k, oneIdx)]
+        
+        
+        
+        
+        # res=[]
+        # for i in range(len(mat)):
+        #     res.append([i,sum(mat[i])])
+        # final= sorted(res,key=lambda x:x[1])
+        # return [i[0]for i in final][:k]
+    
+    
+    
+#         scores = list(map(lambda l: sum(l), mat))
+#         scores_enum = list(enumerate(scores))
+#         print(scores_enum)
+        
+#         ordered = sorted(scores_enum, key=lambda el: el[1])
+        
+#         return [ordered[i][0] for i in range(k)]
+
+        
