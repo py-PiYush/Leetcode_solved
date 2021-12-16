d2b617283276ac859ae48f623ce2bf01b31886c2
Piyush <impiyush016@gmail.com>
2021-12-16 04:16:28 +0000

    Sync LeetCode submission - Permutations (python3)

--- problems/permutations/solution.py
+++ problems/permutations/solution.py
@@ -1,4 +1,19 @@
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
-        from itertools import permutations
-        return list(permutations(nums))
+        
+        
+        
+        # from itertools import permutations
+        # return list(permutations(nums))
+        
+        def backtrack(start, end):
+            if start == end:
+                ans.append(nums[:])
+            for i in range(start, end):
+                nums[start], nums[i] = nums[i], nums[start]
+                backtrack(start+1, end)
+                nums[start], nums[i] = nums[i], nums[start]
+                
+        ans = []
+        backtrack(0, len(nums))
+        return ans
