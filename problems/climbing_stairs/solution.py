ae63099de603f3a207739ed53def175dfe792441
Piyush <impiyush016@gmail.com>
2021-12-17 03:58:14 +0000

    Sync LeetCode submission - Climbing Stairs (python3)

--- problems/climbing_stairs/solution.py
+++ problems/climbing_stairs/solution.py
@@ -1,10 +1,23 @@
 class Solution:
-    def climbStairs(self, n: int) -> int:
-        def fibonacci(n):
-            if n==0 or n==1:
-                return n
-            a,b=0,1
-            for _ in range(n):
-                a,b=b,b+a
-            return a
-        return fibonacci(n+1)
+    
+    
+    def __init__(self):
+        self.dic = {1:1, 2:2}
+    
+    def climbStairs(self, n):
+        if n not in self.dic:
+            self.dic[n] = self.climbStairs(n-1) + self.climbStairs(n-2)
+        return self.dic[n]
+    
+    
+    
+    
+    # def climbStairs(self, n: int) -> int:
+    #     def fibonacci(n):
+    #         if n==0 or n==1:
+    #             return n
+    #         a,b=0,1
+    #         for _ in range(n):
+    #             a,b=b,b+a
+    #         return a
+    #     return fibonacci(n+1)
