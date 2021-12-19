823c3495eb3c33972f168368a223d3cfac15f645
Piyush <impiyush016@gmail.com>
2021-12-19 15:14:55 +0000

    Sync LeetCode submission - Relative Sort Array (python3)

---
+++ problems/relative_sort_array/solution.py
@@ -0,0 +1,12 @@
+class Solution:
+    def relativeSortArray(self, arr1: List[int], arr2: List[int]) -> List[int]:
+        cnt=Counter(arr1)
+        res=[]
+        srt=[]
+        for i in arr2:
+            res.extend([i]*cnt[i])
+        for i in arr1:
+            if i not in arr2:
+                srt.append(i)
+        srt.sort()
+        return res+srt
