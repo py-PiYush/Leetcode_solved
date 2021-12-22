412c2209d29ab2195fa7768c8edecc6cd99f940a
Piyush <impiyush016@gmail.com>
2021-12-22 12:09:06 +0000

    Sync LeetCode submission - Smallest Subsequence of Distinct Characters (python3)

---
+++ problems/smallest_subsequence_of_distinct_characters/solution.py
@@ -0,0 +1,12 @@
+class Solution:
+    def smallestSubsequence(self, s: str) -> str:
+        
+        last={c:i for i,c in enumerate(s)}
+        stack=[]
+        for i,c in enumerate(s):
+            if c in stack: continue
+            while stack and stack[-1]>c and i<last[stack[-1]]:
+                print(stack)
+                stack.pop()
+            stack.append(c)
+        return ''.join(stack)
