316dd3cb115f488df1b5c0f740644724024efe88
Piyush <impiyush016@gmail.com>
2022-01-01 15:22:48 +0000

    Sync LeetCode submission - 4Sum (python3)

--- problems/4sum/solution.py
+++ problems/4sum/solution.py
@@ -1,6 +1,31 @@
 class Solution:
     def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
         
+        def findNsum(l, r, target, N, result, results):
+            if r-l+1 < N or N < 2 or target < nums[l]*N or target > nums[r]*N:  # early termination
+                return
+            if N == 2: # two pointers solve sorted 2-sum problem
+                while l < r:
+                    s = nums[l] + nums[r]
+                    if s == target:
+                        results.append(result + [nums[l], nums[r]])
+                        l += 1
+                        while l < r and nums[l] == nums[l-1]:
+                            l += 1
+                    elif s < target:
+                        l += 1
+                    else:
+                        r -= 1
+            else: # recursively reduce N
+                for i in range(l, r+1):
+                    if i == l or (i > l and nums[i-1] != nums[i]):
+                        findNsum(i+1, r, target-nums[i], N-1, result+[nums[i]], results)
+
+        nums.sort()
+        results = []
+        findNsum(0, len(nums)-1, target, 4, [], results)
+        return results
+        
         def kSum(nums, target, k):
             res=[]
             if not nums:
@@ -26,7 +51,7 @@ class Solution:
                 cur_sum=nums[lo]+nums[hi]
                 if cur_sum<target or (lo>0 and nums[lo]==nums[lo-1]):
                     lo+=1
-                elif cur_sum>target or (hi< len(nums)-1 and nums[hi]==nums[hi]+1):
+                elif cur_sum>target or (hi< len(nums)-1 and nums[hi]==nums[hi+1]):
                     hi-=1
                 else:
                     res.append([nums[lo], nums[hi]])
