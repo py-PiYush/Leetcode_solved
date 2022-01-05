335d839eeae1d951f7f03c76c521188e888436e0
Piyush <impiyush016@gmail.com>
2022-01-05 09:20:51 +0000

    Sync LeetCode submission - Subarray Sum Equals K (python3)

--- problems/subarray_sum_equals_k/solution.py
+++ problems/subarray_sum_equals_k/solution.py
@@ -11,3 +11,17 @@ class Solution:
                 ans+=countPref[need]
             countPref[pref]=countPref.get(pref,0)+1
         return ans
+    
+    
+        ''' prefix sum (prefixSum[i->j] = prefixSum[j]-prefixSum[i-1])
+            
+            TLE
+        '''
+        cnt=0
+        for i in range(len(nums)):
+            pref=0
+            for j in range(i,  len(nums)):
+                pref+=nums[j]
+                if pref==k:
+                    cnt+=1
+        return cnt
