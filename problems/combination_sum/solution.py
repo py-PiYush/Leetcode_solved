f224840879df0f1f991ed6246fd720a685d776e7
Piyush <impiyush016@gmail.com>
2022-01-05 11:46:50 +0000

    Sync LeetCode submission - Combination Sum (python3)

--- problems/combination_sum/solution.py
+++ problems/combination_sum/solution.py
@@ -1,17 +1,33 @@
 class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
         
-        def helper(idx, target, res):
-            if idx==len(candidates) and target==0:
-                ans.append(res[:])
-                return 
-            if idx==len(candidates):
-                return
-            elem=candidates[idx]
-            if elem<=target:
-                helper(idx, target-elem, res+[elem])
-            helper(idx+1, target, res)
         
+        def helper(idx, target, res):
+            if target==0:
+                ans.append(res[:])
+                return
+            for i in range(idx, len(candidates)):
+                if candidates[i]>target:
+                    break
+                helper(i, target-candidates[i], res+[candidates[i]])
+        
+        candidates.sort()
         ans=[]
         helper(0,target, [])
         return ans
+                
+        
+#         def helper(idx, target, res):
+#             if idx==len(candidates) and target==0:
+#                 ans.append(res[:])
+#                 return 
+#             if idx==len(candidates):
+#                 return
+#             elem=candidates[idx]
+#             if elem<=target:
+#                 helper(idx, target-elem, res+[elem])
+#             helper(idx+1, target, res)
+        
+#         ans=[]
+#         helper(0,target, [])
+#         return ans
