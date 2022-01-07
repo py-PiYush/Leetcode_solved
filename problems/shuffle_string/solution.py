4dce728d8dd4f184d7ac262cc22302a2686d7b3b
Piyush <impiyush016@gmail.com>
2022-01-07 15:42:24 +0000

    Sync LeetCode submission - Shuffle String (python3)

--- problems/shuffle_string/solution.py
+++ problems/shuffle_string/solution.py
@@ -1,6 +1,25 @@
 class Solution:
     def restoreString(self, s: str, indices: List[int]) -> str:
-        newarr=zip(indices,list(s))
-        res=sorted(newarr,key=lambda x:x[0])
-        shfld=''.join([i[1] for i in res])
-        return shfld
+        
+        ''' O(n) time and space'''
+        res=[0]*len(s)
+        for i in range(len(s)):
+            res[indices[i]] = s[i]
+            
+        return ''.join(res)
+        
+        ''' Sorting '''
+        return ''.join([x[1] for x in sorted(list(zip(indices, list(s))))])
+    
+    
+        '''Cyclic Sort'''
+        s = list(s)
+        cur = 0
+        while cur < len(s):
+            if cur == indices[cur]:
+                cur += 1
+                continue
+            newIndex = indices[cur]
+            s[cur], indices[cur], s[newIndex], indices[newIndex] = s[newIndex], indices[newIndex] , s[cur], indices[cur]
+        return "".join(s)
+                
