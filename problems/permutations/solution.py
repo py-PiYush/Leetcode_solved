ffd068553118ec89a80070a767a665b9bac297a0
Piyush <impiyush016@gmail.com>
2022-01-10 14:19:11 +0000

    Sync LeetCode submission - Permutations (python3)

--- problems/permutations/solution.py
+++ problems/permutations/solution.py
@@ -5,6 +5,21 @@ class Solution:
         
         # from itertools import permutations
         # return list(permutations(nums))
+        res=[]
+        self.dfs(nums, [], res)
+        return res
+    
+    def dfs(self,nums, path, res):
+        if not nums:
+            res.append(path)
+            return
+        for i in range(len(nums)):
+            #removing nums[i] from arr
+            self.dfs(nums[:i]+nums[i+1:], path+[nums[i]], res)
+            
+        
+        
+        
         
         def backtrack(start, end):
             if start == end:
