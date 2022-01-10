fc6887f30570f407b4551a526d96b1531ee06915
Piyush <impiyush016@gmail.com>
2022-01-10 14:50:47 +0000

    Sync LeetCode submission - Permutations II (python3)

---
+++ problems/permutations_ii/solution.py
@@ -0,0 +1,36 @@
+class Solution:
+    def permuteUnique(self, nums: List[int]) -> List[List[int]]:
+        
+        
+        '''sorting to avoid duplicates'''
+        def dfs(nums, path):
+            if not nums:
+                ans.append(path[:])
+                return 
+            
+            for i in range(len(nums)):
+                if i>0 and nums[i]==nums[i-1]:
+                    continue
+                dfs(nums[:i]+nums[i+1:], path+[nums[i]])
+                
+        ans=[]
+        dfs(nums, [])
+        
+        
+        '''Hash table'''
+        count=Counter(nums)
+        ans=[]
+        
+        def dfs(path):
+            if len(path)==len(nums):
+                ans.append(path[:])
+                return 
+            
+            for n in count:
+                if count[n]>0:
+                    count[n]-=1
+                    dfs(path+[n])
+                    count[n]+=1
+            
+        dfs([])
+        return ans
