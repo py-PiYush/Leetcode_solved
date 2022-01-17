b1581978ef0da84fc703e881e05d1f5802b33e64
Piyush <impiyush016@gmail.com>
2022-01-17 09:55:24 +0000

    Sync LeetCode submission - Climbing Stairs (python3)

--- problems/climbing_stairs/solution.py
+++ problems/climbing_stairs/solution.py
@@ -5,6 +5,18 @@ class Solution:
         self.dic = {1:1, 2:2}
     
     def climbStairs(self, n):
+        
+        '''Bottom up dp'''
+        if n==1: return 1
+        dp=[0]*(n+1)
+        dp[1] = 1
+        dp[2] = 2
+        for i in range(3, n+1):
+            dp[i]=dp[i-1]+dp[i-2]
+        return dp[n]
+        
+        
+        '''Top down dp'''
         if n not in self.dic:
             self.dic[n] = self.climbStairs(n-1) + self.climbStairs(n-2)
         return self.dic[n]
