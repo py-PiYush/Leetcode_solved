eb4825574d9aabfe81df928045b62bd888d625ce
Piyush <impiyush016@gmail.com>
2022-01-17 10:50:49 +0000

    Sync LeetCode submission - House Robber (python3)

--- problems/house_robber/solution.py
+++ problems/house_robber/solution.py
@@ -1,6 +1,39 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        @cache
-        def rob(i):
-            return max(rob(i+1), nums[i] + rob(i+2)) if i < len(nums) else 0
-        return rob(0)
+        '''Space optimized'''
+        cur, prev, prev2=0,0,0
+        for n in nums:
+            cur=max(prev, n+prev2)
+            prev2=prev
+            prev=cur
+        return cur
+        
+        
+        ''' Bottom up'''
+        if len(nums)==1:
+            return nums[0]
+        dp=[0]*(len(nums))
+        dp[0]=nums[0]
+        dp[1]=max(nums[0], nums[1])
+        for i in range(2, len(nums)):
+            dp[i]=max(dp[i-1], dp[i-2]+nums[i])
+        return dp[len(nums)-1]
+        
+        
+        
+        
+        '''Top down'''
+        def dp(i):
+            #Base cases
+            if i==0:
+                return nums[0]
+            if i==1:
+                return max(nums[0], nums[1])
+            if i not in memo:
+                memo[i]=max(dp(i-1), dp(i-2)+nums[i])
+            return memo[i]
+        
+        memo={}
+        return dp(len(nums)-1)
+    
+    
