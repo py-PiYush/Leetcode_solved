0e905429884a5a582afcb33c3b0e2a7308999cce
Piyush <impiyush016@gmail.com>
2022-01-21 10:13:26 +0000

    Sync LeetCode submission - Min Cost Climbing Stairs (python3)

--- problems/min_cost_climbing_stairs/solution.py
+++ problems/min_cost_climbing_stairs/solution.py
@@ -1,7 +1,19 @@
 class Solution:
     def minCostClimbingStairs(self, cost: List[int]) -> int:
+        ''' bottom up (space optimized)'''
+        if len(cost)<=2:
+            return min(cost[0], cost[1])
+        one_back, two_back = 0, 0
+        for i in range(2, len(cost)+1):
+            temp=one_back
+            one_back=min(one_back + cost[i-1], two_back + cost[i-2])
+            two_back=temp
+        return one_back
+        
+        
+        
         '''Bottom up (tabulation)'''
-        if len(cost)<=1:
+        if len(cost)<=2:
             return min(cost[0], cost[1])
         dp=[-1]*(len(cost)+1)
         dp[0]=0
