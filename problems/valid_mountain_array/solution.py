82b26f0eaa04e1a05b99df4489214ed5112899fd
Piyush <impiyush016@gmail.com>
2022-01-25 03:02:17 +0000

    Sync LeetCode submission - Valid Mountain Array (python3)

--- problems/valid_mountain_array/solution.py
+++ problems/valid_mountain_array/solution.py
@@ -1,47 +1,28 @@
 class Solution:
-    def validMountainArray(self, arr: List[int]) -> bool:
-        if len(arr)<3:
+    def validMountainArray(self, A: List[int]) -> bool:
+        i, j, n = 0, len(A) - 1, len(A)
+        while i + 1 < n and A[i] < A[i + 1]: i += 1
+        while j > 0 and A[j - 1] > A[j]: j -= 1
+        return 0 < i == j < n - 1
+        
+        
+        
+        
+        '''-------------'''
+        N = len(A)
+        i = 0
+
+        # walk up
+        while i+1 < N and A[i] < A[i+1]:
+            i += 1
+
+        # peak can't be first or last
+        if i == 0 or i == N-1:
             return False
-        
-        for i in range(len(arr)-1):
-            if arr[i]==arr[i+1]:
-                return False
-            
-        pivot=len(arr)
-        for i in range(len(arr)-1):
-            if arr[i]>arr[i+1]:
-                pivot=i
-                break
-        print(pivot)
-        
-        if pivot == len(arr) or pivot == 0:
-            return False
-        
-        for i in range(pivot-1):
-            if arr[i]>arr[i+1]:
-                return False
-            
-        for j in range(pivot,len(arr)-1):
-            if arr[j]<arr[j+1]:
-                return False
-            
-        return True
-    
-                
-        
-#         N = len(A)
-#         i = 0
 
-#         # walk up
-#         while i+1 < N and A[i] < A[i+1]:
-#             i += 1
+        # walk down
+        while i+1 < N and A[i] > A[i+1]:
+            i += 1
 
-#         # peak can't be first or last
-#         if i == 0 or i == N-1:
-#             return False
-
-#         # walk down
-#         while i+1 < N and A[i] > A[i+1]:
-#             i += 1
-
-#         return i == N-1
+        return i == N-1
+        
