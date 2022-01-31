456903471b56684bf17141ddedf2c788fb80fb6e
Piyush <impiyush016@gmail.com>
2022-01-31 15:27:41 +0000

    Sync LeetCode submission - Find if Path Exists in Graph (python3)

---
+++ problems/find_if_path_exists_in_graph/solution.py
@@ -0,0 +1,62 @@
+class UnionFind:
+    def __init__(self, size):
+        self.parent = {i:i for i in range(size)}
+        self.rank = [1]*size
+        
+    def find(self, x):
+        if self.parent[x]==x:
+            return x
+        self.parent[x]=self.find(self.parent[x])
+        return self.parent[x]
+    
+    def union(self, x, y):
+        X=self.find(x)
+        Y=self.find(y)
+        if X==Y: return
+        if self.rank[X]>self.rank[Y]:
+            self.parent[Y]=X
+        elif self.rank[X]<self.rank[Y]:
+            self.parent[X]=Y
+        else:
+            self.parent[X]=Y
+            self.rank[Y]+=1
+        
+    def connected(self, x, y):
+        return self.find(x)==self.find(y)
+            
+
+class Solution:
+    def validPath(self, n: int, edges: List[List[int]], source: int, destination: int) -> bool:
+        ''' Union Find '''
+        uf=UnionFind(n)
+        for a,b in edges:
+            uf.union(a,b)
+        return uf.connected(source, destination)
+        
+        
+        
+        
+        
+        ''' iterative dfs (card sol)'''
+        adj_list=[[] for _ in range(n)]
+        seen=set()
+        
+        for a,b in edges:
+            adj_list[a].append(b)
+            adj_list[b].append(a)
+            
+        stack=[source]
+        
+        while stack:
+            node=stack.pop()
+            if node==destination:
+                return True
+            if node in seen:
+                continue
+            seen.add(node)
+            
+            for neighbour in adj_list[node]:
+                stack.append(neighbour)
+        
+        return False
+                
