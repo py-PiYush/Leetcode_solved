e496b58f850d594a4307ffea2a9de43601918b94
Piyush <impiyush016@gmail.com>
2022-02-03 02:57:14 +0000

    Sync LeetCode submission - 4Sum (python3)

--- problems/4sum/solution.py
+++ problems/4sum/solution.py
@@ -20,44 +20,45 @@ class Solution:
                 for i in range(l, r+1):
                     if i == l or (i > l and nums[i-1] != nums[i]):
                         findNsum(i+1, r, target-nums[i], N-1, result+[nums[i]], results)
-
         nums.sort()
         results = []
         findNsum(0, len(nums)-1, target, 4, [], results)
         return results
         
-        def kSum(nums, target, k):
-            res=[]
-            if not nums:
-                return res
+                                 
+        '''------------------------------------------------------------------'''
+#         def kSum(nums, target, k):
+#             res=[]
+#             if not nums:
+#                 return res
             
-            average=target//k
-            if average<nums[0] or average>nums[-1]:
-                return res
+#             average=target//k
+#             if average<nums[0] or average>nums[-1]:
+#                 return res
             
-            if k==2:
-                return twoSum(nums, target)
+#             if k==2:
+#                 return twoSum(nums, target)
             
-            for i in range(len(nums)):
-                if i==0 or nums[i-1]!=nums[i]:
-                    for subset in kSum(nums[i+1:], target-nums[i], k-1):
-                        res.append([nums[i]]+subset)
-            return res
+#             for i in range(len(nums)):
+#                 if i==0 or nums[i-1]!=nums[i]:
+#                     for subset in kSum(nums[i+1:], target-nums[i], k-1):
+#                         res.append([nums[i]]+subset)
+#             return res
         
-        def twoSum(nums, target):
-            res=[]
-            lo,hi=0, len(nums)-1
-            while lo<hi:
-                cur_sum=nums[lo]+nums[hi]
-                if cur_sum<target or (lo>0 and nums[lo]==nums[lo-1]):
-                    lo+=1
-                elif cur_sum>target or (hi< len(nums)-1 and nums[hi]==nums[hi+1]):
-                    hi-=1
-                else:
-                    res.append([nums[lo], nums[hi]])
-                    lo+=1
-                    hi-=1
-            return res
+#         def twoSum(nums, target):
+#             res=[]
+#             lo,hi=0, len(nums)-1
+#             while lo<hi:
+#                 cur_sum=nums[lo]+nums[hi]
+#                 if cur_sum<target or (lo>0 and nums[lo]==nums[lo-1]):
+#                     lo+=1
+#                 elif cur_sum>target or (hi< len(nums)-1 and nums[hi]==nums[hi+1]):
+#                     hi-=1
+#                 else:
+#                     res.append([nums[lo], nums[hi]])
+#                     lo+=1
+#                     hi-=1
+#             return res
         
-        nums.sort()
-        return kSum(nums, target, 4)
+#         nums.sort()
+#         return kSum(nums, target, 4)
