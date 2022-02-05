c75c4e1f0118a24091b6ab62d55c45e6dda63a0f
Piyush <impiyush016@gmail.com>
2022-02-05 16:33:09 +0000

    Sync LeetCode submission - Partition Array According to Given Pivot (python3)

---
+++ problems/partition_array_according_to_given_pivot/solution.py
@@ -0,0 +1,20 @@
+class Solution:
+    def pivotArray(self, nums: List[int], pivot: int) -> List[int]:
+        n = len(nums)
+        ans = [pivot] * n
+        left, right = 0, -1
+        for i, num in enumerate(nums):
+            if num < pivot:
+                ans[left], left = num, left + 1
+            if nums[~i] > pivot:
+                ans[right], right = nums[~i], right - 1
+        return ans
+        
+        
+        '''----------------------------------------'''
+        lessThan = [x for x in nums if x<pivot]
+        equal = [x for x in nums if x==pivot]
+        greaterThan = [x for x in nums if x>pivot]
+        
+        return lessThan + equal + greaterThan
+            
