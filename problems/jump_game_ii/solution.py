d54b0b64ffd95710a05e06dcf9bb62385c78a206
Piyush <impiyush016@gmail.com>
2022-02-07 09:23:12 +0000

    Sync LeetCode submission - Jump Game II (python3)

--- problems/jump_game_ii/solution.py
+++ problems/jump_game_ii/solution.py
@@ -1,5 +1,7 @@
 class Solution:
     def jump(self, nums: List[int]) -> int:
+        
+        '''Greedy'''
         l,r=0,0
         jumps=0
         while r<len(nums)-1:
@@ -8,3 +10,16 @@ class Solution:
             l,r=r+1,furthest
             
         return jumps
+    
+    
+    
+        ''' DP -> O(n^2)'''
+        n=len(nums)
+        dp=[0]+[float('inf')]*n
+        
+        for i in range(1,n):
+            for j in range(i+1):
+                if i<=j+nums[j]:
+                    dp[i]=min(dp[i], dp[j]+1)
+        # print(dp)
+        return dp[n-1]
