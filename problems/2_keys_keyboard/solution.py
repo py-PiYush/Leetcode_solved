fa252fc023ba4594c5522bddc1c301ef81e027c4
Piyush <impiyush016@gmail.com>
2022-02-08 12:17:35 +0000

    Sync LeetCode submission - 2 Keys Keyboard (python3)

--- problems/2_keys_keyboard/solution.py
+++ problems/2_keys_keyboard/solution.py
@@ -1,6 +1,18 @@
 class Solution:
     def minSteps(self, n: int) -> int:
+        ''' prime factorization '''
+        ans = 0
+        d = 2
+        while n > 1:
+            while n % d == 0:
+                ans += d
+                n /= d
+            d += 1
+        return ans
         
+        
+        
+        ''' dp '''
         dp=[0]*(n+1)
         for i in range(2, n+1):
             dp[i]=i
