de64177f0fa768dc6ddfbda52af14d821f03289f
Piyush <impiyush016@gmail.com>
2022-02-13 03:11:32 +0000

    Sync LeetCode submission - Subsets (python3)

--- problems/subsets/solution.py
+++ problems/subsets/solution.py
@@ -3,7 +3,7 @@ class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
         
         
-        ''''''
+        '''Iterative'''
         ret = [[]]
         for n in nums:
             ret += [r + [n] for r in ret]
