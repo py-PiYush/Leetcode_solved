3e8e8594f98a00340b9c52ffa0f32442cdc960d6
Piyush <impiyush016@gmail.com>
2022-02-17 03:14:47 +0000

    Sync LeetCode submission - Longest Palindromic Substring (python)

--- problems/longest_palindromic_substring/solution.py
+++ problems/longest_palindromic_substring/solution.py
@@ -4,20 +4,21 @@ class Solution(object):
         :type s: str
         :rtype: str
         """
-        self.maxlen = 0
-        self.start = 0
+        res = ""
+        for i in xrange(len(s)):
+            # odd case, like "aba"
+            tmp = self.helper(s, i, i)
+            if len(tmp) > len(res):
+                res = tmp
+            # even case, like "abba"
+            tmp = self.helper(s, i, i+1)
+            if len(tmp) > len(res):
+                res = tmp
+        return res
 
-        for i in range(len(s)):
-            self.expandFromCenter(s,i,i)
-            self.expandFromCenter(s,i,i+1)
-        return s[self.start:self.start+self.maxlen]
-
-
-    def expandFromCenter(self,s,l,r):
-        while l > -1 and r < len(s) and s[l] ==s[r]:
-            l -= 1
-            r += 1
-
-        if self.maxlen < r-l-1:
-            self.maxlen = r-l-1
-            self.start = l + 1
+    # get the longest palindrome, l, r are the middle indexes   
+    # from inner to outer
+    def helper(self, s, l, r):
+        while l >= 0 and r < len(s) and s[l] == s[r]:
+            l -= 1; r += 1
+        return s[l+1:r]
