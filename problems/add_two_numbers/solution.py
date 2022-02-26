8dd24dc015ca4ae153d90e92fb080f0a82e053d1
Piyush <impiyush016@gmail.com>
2022-02-26 14:36:28 +0000

    Sync LeetCode submission - Add Two Numbers (python3)

--- problems/add_two_numbers/solution.py
+++ problems/add_two_numbers/solution.py
@@ -4,27 +4,28 @@
 #         self.val = val
 #         self.next = next
 class Solution:
-    def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
-        carry=0
-        sum_=ListNode(0)
-        head=sum_
-        res=0
-        if l1==None and l2==None and carry==0:
-            return None
-        while l1 or l2:
-            res=carry
-            if l1:
-                res+=l1.val
-                l1=l1.next
-            if l2:
-                res+=l2.val
-                l2=l2.next
-            value=res%10
-            carry=res//10
-            sum_.next=ListNode(value)
-            sum_=sum_.next
-        if carry==1:
-            sum_.next=ListNode(carry)
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        result = head = ListNode(0)
+        carry = 0
+        while l1 and l2: 
+            val = carry + l1.val + l2.val
+            head.next=ListNode(val%10)
+            carry = val//10
+            l1, l2, head = l1.next, l2.next, head.next
         
-        return head.next
+        while l1:
+            val = carry + l1.val
+            head.next = ListNode(val%10)
+            carry = val//10
+            head, l1 = head.next, l1.next
         
+        while l2:
+            val = carry + l2.val
+            head.next = ListNode(val%10)
+            carry = val//10
+            head, l2 = head.next, l2.next
+        
+        if carry:
+            head.next=ListNode(carry)
+        
+        return result.next
