ecff3a625eb88a48f993efe70b521e23eee8397e
Piyush <impiyush016@gmail.com>
2022-02-26 15:27:22 +0000

    Sync LeetCode submission - Integer to Roman (python3)

---
+++ problems/integer_to_roman/solution.py
@@ -0,0 +1,13 @@
+class Solution:
+    def intToRoman(self, num: int) -> str:
+        roman = 'M,CM,D,CD,C,XC,L,XL,X,IX,V,IV,I'.split(',')
+        integers = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1]
+        d=dict(zip(integers, roman ))
+        print(d)
+        
+        res=''
+        for n in integers:
+            res += d[n]*(num//n)
+            num=num%n
+            
+        return res
