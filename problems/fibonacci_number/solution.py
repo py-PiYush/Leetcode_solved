c8d767a09ec83cfb814ce99a12669bcfc85effea
Piyush <impiyush016@gmail.com>
2022-02-27 14:23:44 +0000

    Sync LeetCode submission - Fibonacci Number (python3)

--- problems/fibonacci_number/solution.py
+++ problems/fibonacci_number/solution.py
@@ -1,31 +1,8 @@
 class Solution:
     def fib(self, n: int) -> int:
+        a, b=0, 1
+        if n<=1: return n
+        for _ in range(n):
+            a, b = b, a+b
         
-        
-        '''iterative'''
-        # if n==0 or n==1:
-        #     return n
-        # a,b=0,1
-        # for _ in range(n):
-        #     a,b=b,b+a
-        # return a
-        
-        '''recursive'''
-#         def recurse(n):
-#             if n in cache:
-#                 return cache[n]
-#             result = recurse(n-1)+recurse(n-2)
-#             cache[n]=result
-#             return result
-        
-#         cache={0:0, 1:1}
-#         return recurse(n)
-
-        '''tail recursion'''
-        def rec(n,a=0,b=1):
-            if n==0:
-                return a
-            if n==1:
-                return b
-            return rec(n-1,b,a+b)
-        return rec(n)
+        return a
