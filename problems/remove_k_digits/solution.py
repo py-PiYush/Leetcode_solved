5febe53363fc31a0ee9316c507d40388233468f1
Piyush <impiyush016@gmail.com>
2022-02-28 14:49:39 +0000

    Sync LeetCode submission - Remove K Digits (python3)

--- problems/remove_k_digits/solution.py
+++ problems/remove_k_digits/solution.py
@@ -9,22 +9,3 @@ class Solution:
         if k > 0:
             stack = stack[:-k]     
         return "".join(stack).lstrip("0") or "0"
-        
-        
-        
-        
-        '''Recursion (TLE)'''
-        def helper(s, k):
-            if k==0:
-                return s
-            minimum='9'*(10**5)
-            for i in range(len(s)):
-                n=s[:i]+s[i+1:]
-                if int(n)<int(minimum):
-                    minimum=n
-            return helper(minimum, k-1)
-        
-        if k>=len(num): return '0'
-        ans=helper(num,k)
-        if ans[0]=='0': return str(int(ans)) 
-        return ans
