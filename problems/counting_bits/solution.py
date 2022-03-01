f884167809c8bc3c4ed3f94ee96300d60d3dade4
Piyush <impiyush016@gmail.com>
2022-03-01 02:41:44 +0000

    Sync LeetCode submission - Counting Bits (python3)

--- problems/counting_bits/solution.py
+++ problems/counting_bits/solution.py
@@ -1,5 +1,17 @@
 class Solution:
     def countBits(self, num: int) -> List[int]:
+        '''
+        countBits(N) = countBits(2N)
+        countBits(N)+1 = countBits(2N+1)
+        '''
+        counter = [0]
+        for i in range(1, num+1):
+            counter.append(counter[i >> 1] + i % 2)
+        return counter
+
+        
+        
+        '''-------------'''
         res=[]
         for i in range(num+1):
             cnt=0
