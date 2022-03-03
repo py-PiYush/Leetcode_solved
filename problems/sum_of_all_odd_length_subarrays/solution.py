449c15e476f19e5a414f9054f133b7218426d761
Piyush <impiyush016@gmail.com>
2022-03-03 04:44:47 +0000

    Sync LeetCode submission - Sum of All Odd Length Subarrays (python3)

--- problems/sum_of_all_odd_length_subarrays/solution.py
+++ problems/sum_of_all_odd_length_subarrays/solution.py
@@ -2,15 +2,9 @@
 class Solution:
     def sumOddLengthSubarrays(self, arr: List[int]) -> int:
         sum_=0
-        i=0
-        j=0
-        while i<len(arr):
-            j=i
-            while j<len(arr):
-                sum_+=(sum(arr[i:j+1]))
-                j+=2
-            i+=1
+        for i in range(len(arr)):
+            for j in range(i, len(arr), 2):
+                sum_+=sum(arr[i:j+1])
         return sum_
-        
             
         
