41477b6cca5d0cfa18fd046aca16bef2aed1b2bd
Piyush <impiyush016@gmail.com>
2022-03-04 04:52:14 +0000

    Sync LeetCode submission - Merge Strings Alternately (python3)

--- problems/merge_strings_alternately/solution.py
+++ problems/merge_strings_alternately/solution.py
@@ -1,5 +1,11 @@
 class Solution:
     def mergeAlternately(self, word1: str, word2: str) -> str:
+        return ''.join(a+b for a, b in zip_longest(word1, word2, fillvalue=''))        
+        
+        
+        
+        
+        '''------------'''
         p1=0
         new=''
         while p1<len(word1) and p1<len(word2):
