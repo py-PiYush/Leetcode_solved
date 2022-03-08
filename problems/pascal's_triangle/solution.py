04e22e7a7265e21e7e7ea4f00fb0406a2def1f18
Piyush <impiyush016@gmail.com>
2022-03-08 15:07:04 +0000

    Sync LeetCode submission - Pascal's Triangle (python3)

--- problems/pascal's_triangle/solution.py
+++ problems/pascal's_triangle/solution.py
@@ -1,5 +1,6 @@
 class Solution:
     def generate(self, numRows: int) -> List[List[int]]:
+        
         result=[[1]]
         
         for _ in range(1,numRows):
@@ -8,4 +9,18 @@ class Solution:
             result.append([temp1[i]+temp2[i] for i in range(len(temp1))])
         return result[:numRows]
             
+        
+        '''----------------------------'''
+        res=[]
+        for i in range(numRows):
+            lst=[]
+            for j in range(i+1):
+                if j==0 or j==i:
+                    lst.append(1)
+                else:
+                    lst.append(res[-1][j-1]+res[-1][j])
+            res.append(lst[:])
+        return res
+        
+        
             
