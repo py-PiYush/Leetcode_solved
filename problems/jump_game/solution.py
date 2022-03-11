f2ff2bd77f9ec6ecd2abb57493bad495e23ce99b
Piyush <impiyush016@gmail.com>
2022-03-11 03:32:30 +0000

    Sync LeetCode submission - Jump Game (python3)

--- problems/jump_game/solution.py
+++ problems/jump_game/solution.py
@@ -6,4 +6,20 @@ class Solution:
                 return False
             j=max(j,i+x)
         return True
+        
+        
+        '''--------------------------'''
+        n = len(nums)
+        
+        @lru_cache(None)
+        def dp(i):
+            if i == n - 1:
+                return True
+            
+            for j in range(i+1, min(i+nums[i], n-1) + 1):
+                if dp(j):
+                    return True
+            return False
+        
+        return dp(0)
             
