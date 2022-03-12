5d852601e00bd3ba5014971dc7401f3368748791
Piyush <impiyush016@gmail.com>
2022-03-12 10:49:10 +0000

    Sync LeetCode submission - Partition Equal Subset Sum (python3)

--- problems/partition_equal_subset_sum/solution.py
+++ problems/partition_equal_subset_sum/solution.py
@@ -1,5 +1,28 @@
 class Solution:
     def canPartition(self, nums: List[int]) -> bool:
+        
+        '''--------- Bitmask -------------'''
+        total = sum(nums)
+        if total & 1 : return False
+        target, dp = total // 2, 1
+        
+        for num in nums:
+            dp |= dp<<num
+        return dp & 1<<target
+
+        
+        
+        '''---------- DP O(n) space -------'''
+        total, n = sum(nums), len(nums)
+        if total & 1: return False
+        target = total // 2
+        
+        dp = [True] + [False]*target
+        for x in nums:
+            for s in range(target, x-1, -1):
+                dp[s] = dp[s] or dp[s-x]
+        return dp[target]
+    
         # target, n = sum(nums), len(nums)
         # if target & 1: return False
         # target >>= 1
@@ -8,9 +31,10 @@ class Solution:
         #     dp = [dp[s] or (s >= x and dp[s-x]) for s in range(target+1)]
         #     if dp[target]: return True
         # return False
+        
 
     
-        '''------------------'''
+        '''------- DP O(N^2)-space -----------'''
         total, n = sum(nums), len(nums)
         
         #check if total is odd (equal sum partition not possible)
