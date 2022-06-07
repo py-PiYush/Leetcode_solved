1a8cd2d5bbe2d841d4852e64aaa9443674379124
Piyush <impiyush016@gmail.com>
2022-06-07 12:13:15 +0000

    Sync LeetCode submission - Set Matrix Zeroes (python3)

--- problems/set_matrix_zeroes/solution.py
+++ problems/set_matrix_zeroes/solution.py
@@ -3,28 +3,59 @@ class Solution:
         """
         Do not return anything, modify matrix in-place instead.
         """
-        '''O(1) space'''
         m,n=len(matrix[0]), len(matrix)
-        r1=any(matrix[0][j]==0 for j in range(m))
-        c1=any(matrix[i][0]==0 for i in range(n))
-        
-        for i in range(1,n):
-            for j in range(1,m):
+        row0=1
+        for i in range(n):
+            for j in range(m):
                 if matrix[i][j]==0:
-                    matrix[0][j]=0
-                    matrix[i][0]=0
+                    matrix[0][j] = 0
+                    if i!=0:
+                        matrix[i][0] = 0 
+                    else:
+                        row0=0
         
         for i in range(1,n):
-            for j in range(1,m):
-                if matrix[0][j]*matrix[i][0]==0:
+            for j in range(1, m):
+                if matrix[i][0]*matrix[0][j]==0:
                     matrix[i][j]=0
         
-        if r1:
-            for j in range(m):
-                matrix[0][j]=0
-        if c1:
+        # print(matrix)
+        if matrix[0][0]==0:
             for i in range(n):
                 matrix[i][0]=0
+        # print(matrix)
+        if row0==0:
+            for j in range(m):
+                matrix[0][j]=0
+        # print(matrix)
+                
+
+        
+        
+        
+        
+        '''O(1) space'''
+#         m,n=len(matrix[0]), len(matrix)
+#         r1=any(matrix[0][j]==0 for j in range(m))
+#         c1=any(matrix[i][0]==0 for i in range(n))
+        
+#         for i in range(1,n):
+#             for j in range(1,m):
+#                 if matrix[i][j]==0:
+#                     matrix[0][j]=0
+#                     matrix[i][0]=0
+        
+#         for i in range(1,n):
+#             for j in range(1,m):
+#                 if matrix[0][j]*matrix[i][0]==0:
+#                     matrix[i][j]=0
+        
+#         if r1:
+#             for j in range(m):
+#                 matrix[0][j]=0
+#         if c1:
+#             for i in range(n):
+#                 matrix[i][0]=0
         
         
         '''O(m+n) space'''
