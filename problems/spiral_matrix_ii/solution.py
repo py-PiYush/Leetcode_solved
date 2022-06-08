af438cfdf071a0f61f6b5e1c73cfd2dc819af882
Piyush <impiyush016@gmail.com>
2022-06-08 04:27:36 +0000

    Sync LeetCode submission - Spiral Matrix II (python3)

--- problems/spiral_matrix_ii/solution.py
+++ problems/spiral_matrix_ii/solution.py
@@ -10,6 +10,43 @@ class Solution:
         
         
         ''' tweaks in spiral matrix'''
+        res = [[0]*n for _ in range(n)]
+        left, right, top, bottom = 0, n-1, 0, n-1
+        fill = 0
+        direction = 0
+        
+        while left<=right and top<=bottom:
+            if direction == 0:
+                for i in range(left, right+1):
+                    fill+=1
+                    res[top][i]=fill
+                top+=1
+                    
+            elif direction == 1:
+                for i in range(top, bottom+1):
+                    fill+=1
+                    res[i][right] = fill
+                right-=1
+                
+            elif direction == 2:
+                for i in range(right, left-1, -1):
+                    fill+=1
+                    res[bottom][i]=fill
+                bottom-=1
+            
+            else:
+                for i in range(bottom, top-1, -1):
+                    fill+=1
+                    res[i][left]=fill
+                left+=1
+            
+            direction=(direction+1)%4
+            
+        return res
+        
+        
+        
+        '''----------------------------'''
         res=[[0 for _ in range(n)] for _ in range(n)]
         compass = [(0,1), (1,0), (0,-1), (-1, 0)]
         direction=0
