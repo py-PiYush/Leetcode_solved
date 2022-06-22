7f98d8db48fc81dbaeab46840caa6da384638253
Piyush <impiyush016@gmail.com>
2022-06-22 06:09:16 +0000

    Sync LeetCode submission - Number of Islands (python3)

--- problems/number_of_islands/solution.py
+++ problems/number_of_islands/solution.py
@@ -2,6 +2,18 @@ from collections import deque
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
         
+        '''DFS'''
+        if not grid: return 0
+        count = 0
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if grid[i][j]=='1':
+                    count+=1
+                    self.dfs(grid,i,j)
+        return count
+        
+        
+        ''' BFS '''
         count = 0
         queue = deque([])
         for i in range(len(grid)):
@@ -21,4 +33,13 @@ class Solution:
                 if 0<= i < len(grid) and 0 <= j < len(grid[0]) and grid[i][j] == '1':
                     queue.append((i,j))
                     grid[i][j] = 0 #0
+                    
+    def dfs(self,grid,i,j):
+        if i<0 or j<0 or i>=len(grid) or j>=len(grid[0]) or grid[i][j] != '1':
+            return
+        grid[i][j] = '#'
+        self.dfs(grid, i+1, j)
+        self.dfs(grid, i-1, j)
+        self.dfs(grid, i, j+1)
+        self.dfs(grid, i, j-1)
                     
