ddbd41368d00cfc199c38e602b86e54c27961fb8
Piyush <impiyush016@gmail.com>
2022-06-26 12:20:15 +0000

    Sync LeetCode submission - Letter Case Permutation (python3)

--- problems/letter_case_permutation/solution.py
+++ problems/letter_case_permutation/solution.py
@@ -1,5 +1,18 @@
 class Solution:
     def letterCasePermutation(self, s: str) -> List[str]:
+        def recur(idx, res):
+            if idx==len(s):
+                ans.append( res)
+                return
+            if s[idx].isalpha():
+                recur(idx+1,res+s[idx].upper())
+                recur(idx+1, res+s[idx].lower())
+            else:
+                recur(idx+1,res+s[idx] )
+        ans = []
+        recur(0,'')
+        return ans
+        
         res = ['']
         for ch in s:
             if ch.isalpha():
