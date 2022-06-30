f6e6e8268a25afed4f1f939f5ff6cc3438d1f372
Piyush <impiyush016@gmail.com>
2022-06-30 02:34:17 +0000

    Sync LeetCode submission - Minimum Moves to Equal Array Elements II (python3)

---
+++ problems/minimum_moves_to_equal_array_elements_ii/solution.py
@@ -0,0 +1,6 @@
+class Solution:
+    def minMoves2(self, nums: List[int]) -> int:
+        n = len (nums)
+        mid = sorted (nums) [n // 2]
+        res = sum (abs (i - mid) for i in nums)
+        return res
