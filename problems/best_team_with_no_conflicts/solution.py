96a3aa8cd63ef5085f97b13591207f70833c288d
Piyush <impiyush016@gmail.com>
2022-07-14 06:35:19 +0000

    Sync LeetCode submission - Best Team With No Conflicts (python3)

--- problems/best_team_with_no_conflicts/solution.py
+++ problems/best_team_with_no_conflicts/solution.py
@@ -1,35 +1,36 @@
 class Solution:
     def bestTeamScore(self, scores: List[int], ages: List[int]) -> int:
-        @lru_cache(None)
-        def recurse(idx):
-            res = clubbed[idx][1]
-            for i in range(idx+1, len(ages)):
-                if clubbed[idx][1]>=clubbed[i][1]:
-                    res = max(res, recurse(i) + clubbed[idx][1])
-            return res
+#         @lru_cache(None)
+#         def recurse(idx):
+#             res = clubbed[idx][1]
+#             for i in range(idx+1, len(ages)):
+#                 if clubbed[idx][1]>=clubbed[i][1]:
+#                     res = max(res, recurse(i) + clubbed[idx][1])
+#             return res
         
         
-        clubbed = sorted(zip(ages, scores), key = lambda x: (-x[0], -x[1]))
-        # print(clubbed)
-        ans = 0
-        for i in range(len(ages)):
-            ans=max(ans, recurse(i))
-        return ans
+#         clubbed = sorted(zip(ages, scores), key = lambda x: (-x[0], -x[1]))
+#         # print(clubbed)
+#         ans = 0
+#         for i in range(len(ages)):
+#             ans=max(ans, recurse(i))
+#         return ans
     
-        ''''''
-        @lru_cache(None)
+        '''----------------------------------------'''
+        # @lru_cache(None)
         def recur(idx, maxAgeSoFar):
-            # nonlocal score
             if idx==len(scores):
                 return 0
+            if memo[idx][maxAgeSoFar]!=-1:
+                return memo[idx][maxAgeSoFar]
             if clubbed[idx][1]>=maxAgeSoFar:
-                score=max(clubbed[idx][0]+recur(idx+1, clubbed[idx][1]), recur(idx+1, maxAgeSoFar))
+                memo[idx][maxAgeSoFar]=max(clubbed[idx][0]+recur(idx+1, clubbed[idx][1]), recur(idx+1, maxAgeSoFar))
             else:
-                score=recur(idx+1, maxAgeSoFar)
-            return score
+                memo[idx][maxAgeSoFar]=recur(idx+1, maxAgeSoFar)
+            return memo[idx][maxAgeSoFar]
         
         clubbed = sorted(zip(scores, ages))
-        score = 0
+        memo=[[-1 for _ in range(1001)] for __ in range(len(ages))]
         return recur(0,0)
         
         
