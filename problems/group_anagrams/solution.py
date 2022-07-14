11678a3b1026a4eafdcd79d7b9db9c10a9c4b034
Piyush <impiyush016@gmail.com>
2022-07-14 14:33:39 +0000

    Sync LeetCode submission - Group Anagrams (python3)

--- problems/group_anagrams/solution.py
+++ problems/group_anagrams/solution.py
@@ -1,7 +1,8 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         d = {}
-        for w in sorted(strs):
-            key = tuple(sorted(w))
-            d[key] = d.get(key, []) + [w]
-        return d.values()
+        for word in strs:
+            key = ''.join(sorted(word))
+            d[key] = d.get(key, []) + [word]
+        
+        return list(d.values())
